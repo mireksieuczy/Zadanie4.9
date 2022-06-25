2d4aff11df6c5e54b098ca8610d8ce38c2e5465b
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-26 00:48:11 +0200

    Dodaj pliki projektów.

---
+++ Zadanie4.9/Program.cs
@@ -0,0 +1,8 @@
+﻿string tekst;
+Console.WriteLine("Wprowadź tekst");
+tekst = Console.ReadLine();
+int liczbaWyrazow = 1;
+foreach (char litera in tekst)
+    if (litera == ',') liczbaWyrazow++;
+Console.WriteLine("Wprowadzony tekst zawiera {0} wyrazów", liczbaWyrazow);
+Console.ReadKey();
